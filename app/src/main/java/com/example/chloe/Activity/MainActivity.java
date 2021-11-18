9afc71ba13399f8d778b3a263b39ace731c4a600
Fauzan Fiqriansyah <fauzanfiqriansyah126@gmail.com>
2021-11-18 17:40:00 +0700

    Move MainActivity

--- app/src/main/java/com/example/chloe/Activity/MainActivity.java
+++ app/src/main/java/com/example/chloe/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.chloe;
+package com.example.chloe.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.chloe.R;
 
 public class MainActivity extends AppCompatActivity {
 
