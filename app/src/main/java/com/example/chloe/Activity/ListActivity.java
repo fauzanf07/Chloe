a497f201aae37b9c934d9a0bd5533ab3d015479c
Fauzan Fiqriansyah <fauzanfiqriansyah126@gmail.com>
2021-11-20 22:44:21 +0700

    Add List activity_list.xml

---
+++ app/src/main/java/com/example/chloe/Activity/ListActivity.java
@@ -0,0 +1,16 @@
+package com.example.chloe.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chloe.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
--- app/src/main/java/com/example/chloe/Activity/MainActivity.java
+++ app/src/main/java/com/example/chloe/Activity/MainActivity.java
@@ -3,20 +3,32 @@ package com.example.chloe.Activity;
 import static com.example.chloe.R.color.dark_green;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.chloe.R;
+import com.example.chloe.Splash.SplashScreen;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        CardView indoorDaun = findViewById(R.id.indoor_daun);
+        indoorDaun.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intent);
     }
 }
